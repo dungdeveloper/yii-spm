b31a340ad8bda452d57b8db4c8a2e29979c11cee
Glandore Computer <dungdeveloper@gmail.com>
2012-05-07 12:52:39 +0700

    update code

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -47,7 +47,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by <?php echo Yii::app()->name; ?>.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
