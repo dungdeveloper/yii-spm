9d62798aae2318e6894531a7425296175c7e431c
Glandore Computer <dungdeveloper@gmail.com>
2012-05-17 13:16:21 +0700

    update homepage

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -20,10 +20,6 @@ class SiteController extends Controller {
     }
 
     public function actionIndex() {
-        Yii::app()->user->setFlash('success', '<strong>Well done!</strong> You successfully read this important alert message.');
-        Yii::app()->user->setFlash('info', '<strong>Heads up!</strong> This alert needs your attention, but it\'s not super important.');
-        Yii::app()->user->setFlash('warning', '<strong>Warning!</strong> Best check yo self, you\'re not looking too good.');
-        Yii::app()->user->setFlash('error', '<strong>Oh snap!</strong> Change a few things up and try submitting again.');
         $this->render('index');
     }
 
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,3 +1,23 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
 <?php $this->widget('bootstrap.widgets.BootAlert'); ?>
+
+<?php $this->beginWidget('bootstrap.widgets.BootHero', array(
+    'heading'=>'Sourcing PM!',
+)); ?>
+ 
+    <p>Welcome to Sourcing Project Management!</p>
+    <p><?php $this->widget('bootstrap.widgets.BootButton', array(
+        'type'=>'primary',
+        'size'=>'large',
+        'label'=>'Manage Request',
+        'url'=>array('request/admin'),
+    )); ?>
+    <?php $this->widget('bootstrap.widgets.BootButton', array(
+        'type'=>'primary',
+        'size'=>'large',
+        'label'=>'Manage Project',
+        'url'=>array('project/admin'),
+    )); ?></p>    
+ 
+<?php $this->endWidget(); ?>
